81745079a96d8b576c389780ae55a76dc8c74b69
gokhancelik <gokhancelik987@gmail.com>
2016-12-24 02:10:07 +0300

    added tour detailed component

---
+++ src/app/tour/components/tour-detailed.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'za-tour-detailed',
+    templateUrl: 'tour-detailed.component.html'
+})
+export class TourDetailedComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
