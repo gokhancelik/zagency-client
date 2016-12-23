c4c580e7c0fb243029e9146feb195dbfd3dcc87d
gokhancelik <gokhancelik987@gmail.com>
2016-12-24 01:50:34 +0300

    added theme and header component

---
+++ src/app/theme/theme.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent }   from './header';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [HeaderComponent],
+    providers: [],
+})
+export class ThemeModule { }
