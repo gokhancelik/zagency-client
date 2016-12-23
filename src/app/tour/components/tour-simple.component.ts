cdb0d077bde655e068b2c40335f2e50133aa0961
vahapxxy <clkvhp@gmail.com>
2016-12-24 02:11:28 +0300

    tour simple added

---
+++ src/app/tour/components/tour-simple.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'za-tour-simple',
+    templateUrl: 'tour-simple.component.html'
+})
+export class TourSimpleComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
